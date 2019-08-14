068aa91c2944ae7f00fefd038710dd07fb02288a
Adam Vigneaux <adam@adamvig.com>
2019-08-13 21:04:07 -0400

    Remove logging from AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,8 @@ import { Controller, Get, Res } from '@nestjs/common'
 import { Response } from 'express'
 import { join } from 'path'
 
-import { LogFactory, Logger } from './logger/log-factory'
-
 @Controller()
 export class AppController {
-  private readonly log: Logger
-
-  public constructor(log: LogFactory) {
-    this.log = log.child('AppController')
-    this.log.info('running')
-  }
-
   @Get()
   public root(): object {
     return {
