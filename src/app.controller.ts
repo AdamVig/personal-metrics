55a6ba5c745d2dd808859f701f57924c0aca78e4
Adam Vigneaux <adam@adamvig.com>
2019-10-24 21:22:57 -0400

    Scaffold events feature

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common'
+import { Controller, Get, Res, Post, Render } from '@nestjs/common'
 import { Response } from 'express'
 import { join } from 'path'
 
@@ -14,5 +14,16 @@ export class AppController {
   @Get('docs')
   public docs(@Res() res: Response): void {
     res.sendFile(join(__dirname, '..', 'docs/index.html'))
+  }
+
+  @Get('events')
+  @Render('events')
+  public getEventsPage(): { events: string[] } {
+    return { events: ['foo', 'bar'] }
+  }
+
+  @Post('event/:name')
+  public createEvent(): void {
+    return
   }
 }
